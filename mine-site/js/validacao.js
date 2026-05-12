5dd4103ecbcad2a9e9417d336e8f127356bf1b64
Brenosilvxz <josephbreno72@gmail.com>
2026-05-12 14:57:33 -0300

    criando validação

---
+++ mine-site/js/validacao.js
@@ -0,0 +1,11 @@
+const formulario = document.getElementById("meuFormulario");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const nome = document.getElementById("nome");
+    const telefone = document.getElementById("telefone");
+    const sexo = document.getElementById("sexo")
+
+
+})
